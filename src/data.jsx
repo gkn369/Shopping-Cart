1052e155779dbaffc3f22f50db377084e4b9b9bb
Gaurav Kumar Nayak <gauravnayak369@gmail.com>
2024-06-05 11:35:53 +0530

    first commit

---
+++ src/data.jsx
@@ -0,0 +1,117 @@
+const list = [
+  {
+    id: 1,
+    title: "Realme 12 pro+",
+    author:
+      "5G (Submarine Blue, 8GB RAM, 128GB Storage) | 6.7 120Hz Curved AMOLED Display | 64MP Periscope + 50MP Sony IMX 890 OIS Camera + 8MP",
+    price: 27000,
+    img: "https://m.media-amazon.com/images/I/81X-8B-N30L._SL1500_.jpg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "HP All-in-One PC ",
+    author:
+      "13th Gen Intel Core i5 27 (68.6cm) FHD 16GB RAM, 1TB SSD Intel UMA Graphics, 710 White Wireless Keyboard and Mouse Combo (Windows 11 Home, MSO 21, Shell White, 6.72 Kg)",
+    price: 69990,
+    img: "https://m.media-amazon.com/images/I/71pFI90l-aL._SL1500_.jpg",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Soundcore by Anker",
+    author:
+      "Space One Active Noise Cancelling Bluetooth Headphones with Travel Pouch, 2X Stronger Voice Reduction, 40H ANC Playtime, App Control, LDAC Hi-Res, Ergonomic Design for Comfort Fit",
+    price: 8997,
+    img: "https://m.media-amazon.com/images/I/51+xFDM2H2L._SL1200_.jpg",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "Peter England Men Three Piece Suit",
+    author:
+      "Exquisitely cut in a Slim Fit , this Brown Textured three piece suit from Peter England Elite by Peter England is a smart investment for your style repertoire.",
+    price: 10000,
+    img: "https://m.media-amazon.com/images/I/51dwVVE82HL._SY879_.jpg",
+    amount: 1,
+  },
+  {
+    id: 5,
+    title: "Jompers Men's Silk Blend Straight Kurta Pyjama Set",
+    author:
+      "Self design kurta with churidar, Straight knee length kurta, has a mandarin collar, long sleeves, straight hem, side slits, White solid churidar, drawstring closure",
+    price: 1190,
+    img: "https://m.media-amazon.com/images/I/616+mLNdEwL._SY879_.jpg",
+    amount: 1,
+  },
+  {
+    id: 6,
+    title:
+      "SOLETHREADS Samurai | Casual Sneaker Lightweight Cushioned Lace-up Shoes for Men",
+    author:
+      "A high-flying, slam-dunking, statement-making basketball sneaker ready to pack a powerful punch. Made with premium Italian vegan leather and anti-skid rubber outsole, it brings an all new energy to the game while staying true to the original silhouette.",
+    price: 1799,
+    img: "https://m.media-amazon.com/images/I/716+s+r1GNL._SY695_.jpg",
+    amount: 1,
+  },
+  {
+    id: 7,
+    title:
+      "Skybags Tooper Set of 3 (Cabin+ Medium+Large) Hard Luggage (55+65+75 cm) ",
+    author:
+      "Move in style with Skybags Trooper, a premium hard luggage trolley with a highly trendy print. This lightweight trolley bag is made of durable and scratch resistant ABS material which makes it sturdy, waterproof and impact resistant.",
+    price: 693,
+    img: "https://m.media-amazon.com/images/I/71F3nkS+SML._SL1500_.jpg",
+    amount: 1,
+  },
+  {
+    id: 8,
+    title: "Tommy Hilfiger Bank Analog White Dial Men's Watch-TH1791778",
+    author:
+      "Stay on top of your Style game with this Tommy Hilfiger watch - TH1791778. The watch has a 46mm Blue Dial , Blue Strap and 50m water resistance.Tommy Hilfiger watches epitomise this classic cool American style with a twist.",
+    price: 15250,
+    img: "https://m.media-amazon.com/images/I/81gpFUhv0lL._SY879_.jpg",
+    amount: 1,
+  },
+  {
+    id: 9,
+    title: "JBL Partybox 110 | Wireless Bluetooth Party Speaker| 160W",
+    author:
+      "Monstrous Pro Sound| Dynamic Light Show| Upto 12Hrs Playtime | Built-in Powerbank | Guitar & Mic Support PartyBox App (Black)",
+    price: 8000,
+    img: "https://m.media-amazon.com/images/I/71h2c+bB-fL._SL1500_.jpg",
+    amount: 1,
+  },
+  {
+    id: 10,
+    title:
+      "Samsung 108 cm (43 inches) D Series Crystal 4K Vivid Ultra HD Smart LED TV UA43DUE70BKLXL (Black)",
+    author:
+      "Samsung TV's sleek and elegant design draws you into the pure picture. Crafted with a minimalistic style with a boundless design, your screen looks stunning in every angle.",
+    price: 34000,
+    img: "https://m.media-amazon.com/images/I/81l9vreukoL._SL1500_.jpg",
+    amount: 1,
+  },
+  {
+    id: 11,
+    title:
+      "Canon EOS R10 24.2MP RF-S18-45mm f/4.5-6.3 IS STM Mirrorless Camera (APS-C Sensor,,4K UHD Video) – Black",
+    author:
+      "Effortlessly create stunning content with the Canon EOS R50 Mirrorless Camera. Featuring 4K video capabilities, 24.2 Megapixel. It is the perfect choice for content creators and vloggers.",
+    price: 79990,
+    img: "https://m.media-amazon.com/images/I/81F3HiMFxdL._SL1500_.jpg",
+    amount: 1,
+  },
+  {
+    id: 12,
+    title:
+      "Amazon Echo Dot 4th Gen with clock | Smart speaker with powerful bass, LED display and Alexa (Blue)",
+    author:
+      "Echo Dot is a smart speaker that can be operated by voice - even from distance. Alexa can speak both English & Hindi, and new features are added automatically",
+    price: 5499,
+    img: "https://m.media-amazon.com/images/I/71vabukaITL._SL1500_.jpg",
+    amount: 1,
+  },
+];
+
+export default list;
